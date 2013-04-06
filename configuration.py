4f1b27a77af616419100dcbd9951d71014c0ee4d
Aluminium95 <lopezaliaume@gmail.com>
2013-04-06 11:44:48 +0200

    Ajout de commentaires pertinents !

--- configuration.py
+++ configuration.py
@@ -2,17 +2,10 @@
 #-*- coding: utf-8 -*-
 #
 # Lopez Aliaume
-# 30/03/2013
+# 06/04/2013
 #
 # Un utilitaire de modification
 # des fichiers de configuration
-#
-# TODO:
-#	- utilisation du module fsm 
-#		Démonstration de l'utilisation du module FSM
-#		création des états 
-#		des variables globales
-#		des transitions
 #
 #
 # Ce code a pour vocation d'être simple 
@@ -44,32 +37,51 @@ import fsm
 		
 		États:
 			- sélection fichier
+				* « help »
+				* « entrer »
+				* « liste »
 			- sélection variable
 				+ sélection fichier : « up »
 				+ sélection variable :
 					* « éditer »
 					* « liste »
+					* « ou »
+					* « help »
 """
 
 def fichier_variable (m):
 	""" Éffectue la transition 
 		de la sélection du fichier vers la 
 		sélection de la variable 
+		
+		@m : fsm = la machine qui effectue la transition
+
+		@return : None
 	"""
 	# On prend le fichier sélectionné
+	# dans l'état « menu-fichier »
 	f = fsm.get_state_property (m,"fichier")
 	
 	iconsole.afficher ("Main","Vous avez sélectionné le fichier " + f)
 	iconsole.afficher ("Main","Vous êtes maintenant dans le menu « variable »")
 	
-	# Et on le définit comme le seul et l'unique !
+	# Et on le définit comme une propriété
+	# globale à tous les états (accessible partout)
+	# car les variables d'état sont DÉTRUITES
+	# après chaque transition !
 	fsm.set_special_property (m,"fichier",f)
 	
 def variable_fichier (m):
 	""" Effectue la transition de
 		la sélection des variables à
-		la sélection des fichiers 
+		la sélection des fichiers
+
+		@m : fsm = la machine qui effectue la transition
+
+		@return : None
 	"""
+	# On informe juste que l'on revient dans le menu-fichier ... 
+	# rien de bien complexe 
 	iconsole.afficher ("Main","Vous êtes maintenant dans le menu « fichier »")
 
 if __name__ == '__main__':
@@ -80,11 +92,20 @@ if __name__ == '__main__':
 
 	# On crée une machine comme définit en début de fichier 
 	machine = fsm.new ()
-
+	
+	# On définit les transitions possibles entre les états 
+	# Si les états n'existent pas, fsm les crée !!
+	# « état de départ » -> transition -> « état d'arrivée »
+	# transition est une fonction qui prend en argument
+	# la machine qui effectue la transition, AVANT
+	# que le changement d'état ai lieu !
 	fsm.set_transition (machine, "menu-fichier","menu-variable", fichier_variable)
 	fsm.set_transition (machine, "menu-variable","menu-fichier", variable_fichier)
 	
 	# On définit l'état de départ !
+	# L'état sans rien étant « None »
+	# Aucune fonction de transition n'est déclenchée
+	# (logique ...)
 	fsm.transition (machine, "menu-fichier")
 	
 	# La boucle infinie ... moche !
@@ -106,16 +127,30 @@ if __name__ == '__main__':
 		if req == "quit":
 			break # On sort de la boucle, sauve, et quitte le programme 
 		
-	
+		
+		# Ici on définit les deux menus possibles
+		# En prenant les réactions aux évènements 
+		# En fonction de l'état de la machine
 		if st == "menu-fichier": # Si on est dans le menu-fichier les commandes sont 
 			if req == "list":
+				# On affiche la liste des fichiers 
+				# disponnibles 
 				iconsole.afficher (st, persistance.liste_fichiers ())
 			elif req == "entrer":
-				# Yop, on transite 
-				fsm.transition (machine,"menu-variable")
+				# Yop, on transite vers l'état menu-variable
+				# SI ET SEULEMENT SI l'utilisateur 
+				# a sélectionné un fichier !!!
+				if fsm.get_state_property (machine, "fichier") != "":
+					fsm.transition (machine,"menu-variable")
+				else:
+					iconsole.afficher (st,"Vous n'avez pas sélectionné un fichier ...")
 			elif req == "help":
+				# L'aide affiche la liste des commandes possibles 
 				iconsole.afficher (st, "Les commandes sont : « quit », « list », « entrer », « help » et @Fichier")
 			else: # C'est un nom de fichier 
+				# On suppose que si ce n'est pas une commande, 
+				# c'est un nom de fichier, et on change donc 
+				# le fichier sélectionné
 				fsm.set_state_property (machine,"fichier",req)
 				iconsole.afficher (st,"Vous avez sélectionné " + req)
 				
@@ -128,14 +163,21 @@ if __name__ == '__main__':
 
 			f = fsm.get_special_property (machine, "fichier")
 			if req == "list":
+				# On affiche simplement la liste des variables disponnibles 
 				iconsole.afficher (st, persistance.liste_variables (f))
 			elif req == "up":
+				# On retourne au menu fichier ... tout con 
 				fsm.transition (machine,"menu-fichier")
 			elif req == "ou":
+				# On dit à l'utilisateur où il se trouve, c'est à dire le fichier courant 
 				iconsole.afficher (st, "Le fichier courant est : "+ f)
 			elif req == "help":
+				# On affiche la liste des commandes disponnibles 
 				iconsole.afficher (st, "Les commandes sont : « quit », « ou », « list », « help », « up » et @Variable")
 			else: # C'est une variable 
+				# On suppose que si ce n'est pas une commande c'est une variable 
+				# à modifier ... Donc on demande la nouvelle valeur 
+				# et on modifie ... 
 				iconsole.afficher (st, "Valeur actuelle : " + persistance.get_propriete (f,req))
 				
 				val = iconsole.demander (st,"Nouvelle valeur : ")
