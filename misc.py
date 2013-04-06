e91ef4d707c4748025241a3a04f64d450b75cbc3
Aluminium95 <lopezaliaume@gmail.com>
2013-04-06 12:07:02 +0200

    Commentaires plus petit amélioration

--- misc.py
+++ misc.py
@@ -3,8 +3,19 @@
 #03/04/2013
 #prend une chaine de caractère et le traduit str à bool.
 
-def question_fermee(chaine):
-	if chaine=="oui" or chaine=="o":
+def question_fermee (chaine):
+	""" Dit si la réponse à une question fermée 
+		est oui où non 
+
+		@chaine : string = la réponse à la question 
+
+		@return : bool = True si oui, False si autre chose (attention !)
+	"""
+
+	# chaine.upper () permet de tout mettre en majuscule
+	# ce qui premet à « OuI » de devenir « OUI » et de 
+	# bien correspondre ... On sait jamais !
+	if chaine.upper () == "OUI" or chaine.upper () == "O":
 		return True
 	else:
 		return False
