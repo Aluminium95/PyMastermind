cd2b79d494421a27add8206676fe76f654557050
Aluminium95 <lopezaliaume@gmail.com>
2013-04-07 11:07:31 +0200

    Écoute des évènements

--- igraphique.py
+++ igraphique.py
@@ -5,23 +5,13 @@ import affichage
 import moteur 
 import fsm
 
+from turtle import *
+
+machine = False
 
 def init ():
-	# some initialisation here
-	affichage.truc ()
-
-def to_menu ():
-	affichage.afficher_menu ()
-
-
-def to_choix_code ():
-	affichage.afficher_blabla ()
-
-
-def to_jeu ():
-	affichage.clean ()
-
-def run ():
+	global machine 
+	
 	# Création d'une machine :-)
 	machine = fsm.new ()
 
@@ -31,7 +21,36 @@ def run ():
 	fsm.set_transition (machine, "choix-code", "jeu", to_jeu)
 	fsm.set_transition (machine, "jeu", "menu", to_menu)
 
+def to_menu (m):
+	print "to menu"
+
+def to_choix_code (m):
+	print "to choix code"
+
+def to_jeu (m):
+	print "to jeu"
+
+def dispatcher (x,y):
+	""" Fonction qui récupère les clics turtle 
+		et les dispatche sur les boutons correspondants 
+		
+		@x : int = position x du curseur
+		@y : int = position y du curseur
+
+		@return : None
+	"""
+	global machine 
 	
-	# while bla bla bla 
+	print x,y
+def run ():
 
+	sc = getscreen ()
+	# On crée une fonction anonyme, qui permet de « capturer » la variable machine 
+	# et la donner en argument à dispatcher 
+	sc.onclick (dispatcher)
+	# Boucle indéfiniment !
+	mainloop ()
 
+if __name__ == '__main__':
+	init ()
+	run ()
