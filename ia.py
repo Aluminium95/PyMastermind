04d2f33a24d690c30116f95669d70d2fa42204a9
Yann <yann.desmarais@wanadoo.fr>
2013-04-11 22:11:54 +0300

    première implémentation de Knuth

--- ia.py
+++ ia.py
@@ -7,6 +7,7 @@ import moteur
 import iconsole
 import persistance
 from random import choice # faire un choix aléatoire dans une liste 
+from random import * #j'ai besoin de randint() !
 
 def generer_couleurs_aleatoires ():
 	""" Génère un code aléatoire de couleurs, complètement !
@@ -27,7 +28,34 @@ def generer_couleurs_aleatoires ():
 		sortie.append (choice (lst))
 	
 	return sortie
+	
+def random_choice(S):
+	""" fonction qui permet de choisir une code dans la liste des possibilités générés
+		Je sais pas si generer_couleurs_aleatoires fait la même chose...
+	"""
+    a = randint(0,len(S)-1)
+    return S[a]
 
+def create_list(S):
+    """Créé une liste de toutes les combinaison possibles des objets dans S (une même couleur peux se retrouver plusieurs fois)
+        len(li) = n^(4) à l'issue de cette fonction (où n est len(S))
+
+        @S : list = L'univers, l'ensemble des possibilités, pour le mastermind, le nombre de couleurs
+        @return : list = li la liste de toutes les combinaison
+
+    """
+    li=[]
+    for i in S:
+        temp = [0,0,0,0]
+        temp[0] = i
+        for j in S:
+            temp[1] = j
+            for k in S:
+                temp[2] = k
+                for l in S:
+                    temp[3] = l
+                    li.append(list(temp))
+    return li
 
 # L'IA définit le code 
 def choisir_code (mode="aleatoire"):
@@ -84,6 +112,26 @@ def jouer (mode = "aleatoire"):
 	
 			if r == "perdu" or r == "gagne":
 				return
+	def ia_knuth(S):
+    		li = create_list(S)
+    		proposition = random_choice(li)
+   		sol = moteur.proposer_solution(proposition)
+    		if sol == False:
+			return False
+    		if sol == "gagne":
+        		return "gagne"
+    		if sol == "perdu":
+        		return "perdu"
+#Autant prévenir à partir de là ça par en couille, j'ai juste essayer de faire au plus réutilisable...
+    		else:
+        		for i in li:
+           			temp = moteur.proposer_solution(i)
+            """Ou une autre punaise de fonction qui serait plus appropriée...
+                Dans l'idéal un truc qui me permet de mettre en argument le code et la proposition et qui retourne un tuple uniquement"""
+           			 if type(temp) == tuple:
+                			if temp == sol:
+                				li.remove(i)
+                    
 
 	if mode == "aleatoire":
 		ia_alea ()
