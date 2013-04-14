6c846e7618675ea806cb3b42a41cf1cb89266ef5
mariee <marie.camu@orange.fr>
2013-04-14 17:03:55 +0300

    MISE A JOUR  !!!!!! Tout change dans ce code !
    Regardez moi ce petit effet pour les couleurs x) !

--- regles.py
+++ regles.py
@@ -1,141 +1,167 @@
 # -*- coding: utf-8 -*-
-#règles du jeu (mode facile au mode difficile)
-#05/04/2013
-#regles_du_jeu1 : niveau facile.
-#regles_du_jeu2 : niveau normal.
-#regles_du_jeu3 : niveau difficile.
-# Bien appeler la fonction ainsi : regles_du_jeu1("black") ==> avec "black" ainsi histoire de pourvoir lire les caractères.
+#règles du jeu (mode facile, normal et difficile)
+#014/04/2013
 
-from turtle import*
+from turtle import *
+import persistance
+import couleurs
 
-def regles_du_jeu1(couleur):
-        """Affiche les règles ainsi que les aides du jeu. En mode facil.
-(un fond y est inséré)"""
-        screensize(600,600,"white")
-        bgpic("ff.gif")
-        up()
-        goto(30,220)
-        color(couleur)
-        text="Voici les règles du mastermind mode facil:"
-        write(text, False, "center", ("calibri",16,"underline"))
-        goto(50,150)
-        text2="- Vous devez deviner le code couleur du jeu."
-        write(text2, False, "center", ("calibri",16,"normal"))
-        goto(-132,120)
-        dot(10,"red")
-        goto(80,110)
-        text3=" : Signifie que la couleur est juste et bien placée."
-        write(text3, False, "center", ("calibri",16,"normal"))
-        goto(-132,90)
-        dot(10,"black")
-        goto(90,80)
-        text4=" : Signifie que la couleur est juste mais mal placée."
-        write(text4, False, "center", ("calibri",16,"normal"))
-        goto(9,50)
-        text5="- Il y a 8 couleurs sélectionnables."
-        write(text5, False, "center", ("calibri",16,"normal"))
-        goto(64,20)
-        text6="- Le code à trouver est composé de 4 couleurs."
-        write(text6, False, "center", ("calibri",16,"normal"))
-        goto(69,-10)
-        text7="- Vous avez 10 essais maximum pour le deviner."
-        write(text7, False, "center", ("calibri",16,"normal"))
-        goto(50,-40)
-        color("red")
-        begin_fill()
-        text8="Attention,"
-        write(text8, False, "center", ("calibri",16,"normal"))
-        end_fill()
-        color("black")
-        goto(80,-60)
-        text9="si rien n'est indiqué, alors votre réponse est fausse."
-        write(text9, False, "center", ("calibri",16,"normal"))
+persistance.init ()
+couleurs.init ()
 
 
-def regles_du_jeu2(couleur):
+def texte_normal (text):
+    write(text, False, "center", ("calibri",16,"normal"))
+
+
+def texte_petit (text):
+    write(text, False, "center", ("calibri",12,"normal"))
+
+def niveau(niveau):
+    text="Voici les regles du mastermind mode " + niveau + ":"
+    write(text, False, "center", ("calibri",16,"underline"))
+
+def main_text():
+    goto(50,150)
+    text2="- Vous devez deviner le code couleur du jeu."
+    texte_normal (text2)
+    goto(-132,120)
+    dot(10,"red")
+    goto(80,110)
+    text3=" : Signifie que la couleur est juste et bien placée."
+    texte_normal (text3)
+    goto(-132,90)
+    dot(10,"black")
+    goto(90,80)
+    text4=" : Signifie que la couleur est juste mais mal placée."
+    texte_normal (text4)
+    goto(9,50)
+    
+
+def nombre_coup(coups):
+    text5="- Il y a " + coups + " couleurs selectionnables." 
+    texte_normal (text5)
+
+def main_text2():
+    text6="- Le code à trouver est composé de 4 couleurs."
+    texte_normal (text6)
+    goto(69,-10)
+    text7="- Vous avez 10 essais maximum pour le deviner."
+    texte_normal (text7)
+    goto(50,-40)
+    color("red")
+    begin_fill()
+    text8="Attention,"
+    texte_normal (text8)
+    end_fill()
+    color("black")
+    goto(80,-60)
+    text9="si rien n'est indiqué, alors votre réponse est fausse."
+    texte_normal (text9)
+    goto(-185,-130)
+    text10="Panel de couleurs :"
+    write(text10, False, "center", ("calibri",15,"underline"))
+    goto(-285,-280)
+
+def carre(taille):
+    c = 0
+    down ()
+    while c<4:
+        forward(taille)
+        left(90)
+        c=c+1
+    up ()
+
+def carre_facile():
+    li = couleurs.liste_couleurs ()
+    print li
+    for i in li:
+        color (couleurs.string_to_hexa (i))
+        begin_fill ()
+        carre(10)
+        end_fill ()
+        left (90)
+        fd (10)
+        texte_petit(i)
+        right(90)
+        fd (38)
+        
+def carre_normal():
+    li = couleurs.liste_couleurs ()
+    print li
+    for i in li:
+        color (couleurs.string_to_hexa (i))
+        begin_fill ()
+        carre(10)
+        end_fill ()
+        left (90)
+        fd (10)
+        texte_petit(i)
+        right(90)
+        fd (38)
+
+def carre_difficile():
+    li = couleurs.liste_couleurs ()
+    print li
+    for i in li:
+        color (couleurs.string_to_hexa (i))
+        begin_fill ()
+        carre(10)
+        end_fill ()
+        left (90)
+        fd (10)
+        texte_petit(i)
+        right(90)
+        fd (38)
+
+def regles_facile(couleur):
+    """Affiche les règles ainsi que les aides du jeu. En mode facile.
+    (un fond y est inséré)"""
+    screensize(600,600,"white")
+    bgpic("ff.gif")
+    up()
+    goto(30,220)
+    color(couleur)
+    niveau('facile')
+    main_text()
+    goto(8,50)
+    nombre_coup('8')
+    goto(64,20)
+    main_text2()
+    carre_facile()
+
+
+def regles_normal(couleur):
         """Affiche les règles ainsi que les aides du jeu. En mode normal.
-(un fond y est inséré)"""
+        (un fond y est inséré)"""
         screensize(600,600,"white")
         bgpic("fn.gif")
         up()
         goto(30,220)
         color(couleur)
-        text="Voici les règles du mastermind mode normal:"
-        write(text, False, "center", ("calibri",16,"underline"))
-        goto(50,150)
-        text2="- Vous devez deviner le code couleur du jeu."
-        write(text2, False, "center", ("calibri",16,"normal"))
-        goto(-132,120)
-        dot(10,"red")
-        goto(80,110)
-        text3=" : Signifie que la couleur est juste et bien placée."
-        write(text3, False, "center", ("calibri",16,"normal"))
-        goto(-132,90)
-        dot(10,"black")
-        goto(90,80)
-        text4=" : Signifie que la couleur est juste mais mal placée."
-        write(text4, False, "center", ("calibri",16,"normal"))
+        niveau('normal')
+        main_text()
         goto(14,50)
-        text5="- Il y a 12 couleurs sélectionnables."
-        write(text5, False, "center", ("calibri",16,"normal"))
+        nombre_coup('12')
         goto(63,20)
-        text6="- Le code à trouver est composé de 4 couleurs."
-        write(text6, False, "center", ("calibri",16,"normal"))
-        goto(69,-10)
-        text7="- Vous avez 10 essais maximum pour le deviner."
-        write(text7, False, "center", ("calibri",16,"normal"))
-        goto(50,-40)
-        color("red")
-        begin_fill()
-        text8="Attention,"
-        write(text8, False, "center", ("calibri",16,"normal"))
-        end_fill()
-        color("black")
-        goto(80,-60)
-        text9="si rien n'est indiqué, alors votre réponse est fausse."
-        write(text9, False, "center", ("calibri",16,"normal"))
+        main_text2()
+        carre_normal()
 
-def regles_du_jeu3(couleur):
+def regles_difficile(couleur):
         """Affiche les règles ainsi que les aides du jeu. En mode difficile.
-(un fond y est inséré)"""
+        (un fond y est inséré)"""
         screensize(600,600,"white")
         bgpic("fd.gif")
         up()
         goto(30,220)
         color(couleur)
-        text="Voici les règles du mastermind mode difficile:"
-        write(text, False, "center", ("calibri",16,"underline"))
-        goto(50,150)
-        text2="- Vous devez deviner le code couleur du jeu."
-        write(text2, False, "center", ("calibri",16,"normal"))
-        goto(-132,120)
-        dot(10,"red")
-        goto(80,110)
-        text3=" : Signifie que la couleur est juste et bien placée."
-        write(text3, False, "center", ("calibri",16,"normal"))
-        goto(-132,90)
-        dot(10,"black")
-        goto(90,80)
-        text4=" : Signifie que la couleur est juste mais mal placée."
-        write(text4, False, "center", ("calibri",16,"normal"))
+        niveau('difficile')
+        main_text()
         goto(14,50)
-        text5="- Il y a 16 couleurs sélectionnables."
-        write(text5, False, "center", ("calibri",16,"normal"))
+        nombre_coup('16')
         goto(63,20)
-        text6="- Le code à trouver est composé de 4 couleurs."
-        write(text6, False, "center", ("calibri",16,"normal"))
-        goto(69,-10)
-        text7="- Vous avez 10 essais maximum pour le deviner."
-        write(text7, False, "center", ("calibri",16,"normal"))
-        goto(50,-40)
-        color("red")
-        begin_fill()
-        text8="Attention,"
-        write(text8, False, "center", ("calibri",16,"normal"))
-        end_fill()
-        color("black")
-        goto(80,-60)
-        text9="si rien n'est indiqué, alors votre réponse est fausse."
-        write(text9, False, "center", ("calibri",16,"normal"))
+        main_text2()
+        carre_difficile()
+
 
 
