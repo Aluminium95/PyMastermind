37c8943e490c2bda0168fe3870fc99bd71699efa
Aluminium95 <lopezaliaume@gmail.com>
2013-04-20 12:05:38 +0200

    Ajout de la gestion d'erreurs !

--- persistance.py
+++ persistance.py
@@ -23,11 +23,20 @@
 
 # EXCEPTIONS 
 
+class EcritureImpossible (Exception):
+	pass
+
 class FichierInvalide (Exception):
 	pass
 
-class VariableInvalide (Exception):
+class CleInvalide (Exception):
 	pass
+
+class ValelurInvalide (Exception):
+	def __init__ (self, fichier, variable):
+		self.fichier = fichier
+		self.variable = variable
+
 
 # FIN EXCEPTIONS	
 
@@ -50,6 +59,8 @@ def liste_variables (fichier):
 		@fichier : string = nom du fichier dans lequel chercher les variables
 		
 		@return : [string ...]
+		
+		@throw : FichierInvalide
 	"""
 	l = False
 	for i in persistant: # Pour chaque fichier 
@@ -58,7 +69,10 @@ def liste_variables (fichier):
 			for j in i[1:]: # On prend tous les éléments du fichier 
 				l.append (j[0]) # On récupère seulement les noms de ceux-ci
 			break # On casse la boucle 
-	return l # On retourne la liste générée (ou False ...)
+	if l == False:
+		raise FichierInvalide
+	else:
+		return l
 
 def charger_fichier (chemin):
 	""" Charge un fichier de configuration
@@ -91,15 +105,16 @@ def get_propriete (chemin,nom):
 		@nom : string = nom de la propriété 
 		
 		@return : string | False
+	
+		@throw : FichierInvalide et CleInvalide
 	"""
 	for p in persistant: # Pour chaque fichier 
 		if p[0] == chemin: # Si le nom correspond 
 			for i in p[1:]: # On regarde les éléments du fichier 
 				if i[0] == nom: # Si le nom correspond 
 					return i[1] # On retourne la valeur !
-			break # Si le nom n'existe pas, pas la peine d'aller plus loin ...
-	
-	return False 
+			raise CleInvalide	
+	raise FichierInvalide
 
 def get_by_value (chemin,val):
 	""" Récupère le nom de la propriété contenant la valeur @val
@@ -107,15 +122,17 @@ def get_by_value (chemin,val):
 		@chemin : string = chemin du fichier dans lequel se trouve la propriété
 		@val : string = valeur de la propriété
 		
-		@return : string | False
+		@return : string
+
+		@throw : FichierInvalide et CleInvalide 
 	"""
 	for p in persistant: # Pour chaque fichier 
 		if p[0] == chemin: # si le chemin correspond 
 			for i in p[1:]: # On regarde chaque élément
 				if i[1] == val: # Si la valeur correspond 
 					return i[0] # On retourne la clé 
-			break # Si la valeur n'existe pas, pas la peine d'aller plus loin 
-	return False
+			raise CleInvalide	
+	return FichierInvalide
 	
 def new_file (chemin):
 	""" Ajoute un fichier virtuel (enregistré plus tard)
@@ -137,15 +154,17 @@ def add_propriete (chemin,nom,val):
 		@nom : string = nom de la propriété
 		@val : string = valeur de la propriété  
 		
-		@return : bool = si ça s'est bien passé 
+		@return : None
+
+		@throw : FichierInvalide
 	"""
 	global persistant 
 	
 	for p in persistant: # Pour chaque fichier 
 		if p[0] == chemin: # Si le nom correspond
 			p.append ([nom,val]) # On ajoute à la fin le coupe [clé,valeur]
-			return True
-	return False
+			return
+	raise FichierInvalide
 
 def set_propriete (chemin,nom,val):
 	""" Définit une propriété, et la crée si elle n'existe pas 
@@ -154,7 +173,9 @@ def set_propriete (chemin,nom,val):
 		@nom : string = nom de la propriété
 		@val : string = valeur de la propriété  
 		
-		@return : bool = si ça s'est bien passé 
+		@return : None
+
+		@throw : Fichier Invalide
 	"""
 	global persistant
 	
@@ -163,15 +184,17 @@ def set_propriete (chemin,nom,val):
 			for i in p[1:]: # On regarde pour chaque élément
 				if i[0] == nom: # Si l'élément a le bon nom
 					i[1] = val # On définit sa valeur 
-					return True
+					return
 			p.append ([nom,val]) # Sinon on ajoute à la fin [clé,valeur]
-			return True
-	return False
-	
+			return 
+	raise FichierInvalide
+
 def save ():
 	""" Enregistre les modifications dans les fichiers
 	
-		@return : bool = si ça s'est bien passé 
+		@return : None
+
+		@throw : EcritureImpossible
 	"""
 	try:
 		for p in persistant: # Pour chaque fichier 
@@ -182,7 +205,7 @@ def save ():
 				f.write ("\n") # Avec un saut de ligne 
 			f.close () # Et on ferme le fichier 
 	except:
-		return False
+		raise EcritureImpossible
 
 def set_default_value (chemin,variable,valeur):
 	""" Définit une valeur par défaut pour la variable
@@ -193,17 +216,19 @@ def set_default_value (chemin,variable,valeur):
 		@valeur : string = valeur de la variable 
 
 		@return : None
+
+		@throw : FichierInvalide
 	"""
 	try: # Cette fonction peut échouer ...
 		for p in persistant: # Pour chaque fichier 
 			if p[0] == chemin: # Si le nom correspond
 				for prop in p[1:]: # On regarde chaque élément
 					if prop[0] == variable: # Si le nom correspond
-						return True # On retourne Vrai (exsite déjà)
+						return # On ne modifie pas !
 				p.append ([variable,valeur]) # S'il n'exsiste pas, on crée le couple [clé,valeur]
-				return True
+				return 
 	except:
-		return False
+		raise FichierInvalide  
 
 def init ():
 	""" fonction d'initialisation du module
