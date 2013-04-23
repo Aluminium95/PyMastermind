ac6b8dee4cdd62201d41b47d256487b12cec6b68
Aluminium95 <lopezaliaume@gmail.com>
2013-04-23 15:48:59 +0200

    Création d'un module qui gère les matrices !

---
+++ matrice.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+
+# Module qui fait des matrices !
+
+def make (n,m,func):
+	""" Crée une matrice (n,m)
+		remplie avec la fonction func
+
+		@n : int = nombre de lignes
+		@m : int = nombre de colonnes
+		@func : fonction = fonction de remplissage
+
+		@return : matrice
+	"""
+	matrice = {}
+	matrice["n"] = n
+	matrice["m"] = m
+	for i in range (0,n):
+		for j in range (0,m):
+			matrice[(i,j)] = func (i,j)
+	return matrice
+
+def parcourir_colonne (matrice,m):
+	for i in range (0,matrice["n"]):
+		yield matrice[(i,m)]
+
+def parcourir_ligne (matrice,n):
+	for j in range (0,matrice["m"]):
+		yield matrice[(n,j)]
+
+def parcourir_lignes (matrice):
+	for i in range (0,matrice["n"]):
+		l = []
+		for j in range (0,matrice["m"]):
+			l.append (matrice[(i,j)])
+		yield l
+
+def parcourir_colonnes (matrice):
+	for j in range (0, matrice["m"]):
+		l = []
+		for i in range (0,matrice["n"]):
+			l.append (matrice[(i,j)])
+		yield l
+	
+def set (matrice,i,j,val):
+	matrice[(i,j)] = val
+
+def get (matrice,i,j):
+	return matrice[(i,j)]
+
+
