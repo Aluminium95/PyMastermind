54688158b9e947b7948d5d3d7c4045a4519b20e6
Aluminium95 <lopezaliaume@gmail.com>
2013-04-26 16:20:06 +0200

    Régénération complète de la documentation

--- matrice.py
+++ matrice.py
@@ -22,24 +22,51 @@ def make (n,m,func):
 	return matrice
 
 def apply (matrice, func):
+	""" Applique une fonction à chaque cellule de la matrice
+		
+		@func : fonction = (i,j,valeur) -> valeur
+		
+		@return : None
+	"""
 	for i in range (0, matrice["n"]):
 		for j in range (0, matrice ["m"]):
 			matrice[(i,j)] = func (i,j,matrice[(i,j)])
 
 def parcourir_matrice (matrice):
+	""" Générateur qui parcours la matrice
+	
+		@return : generator
+	"""
 	for i in range (0,matrice["n"]):
 		for j in range (0, matrice["m"]):
 			yield matrice[(i,j)]
 
 def parcourir_colonne (matrice,m):
+	""" Générateur qui parcour une colonne de la matrice
+		
+		@m : int = la colonne
+		
+		@return : generator
+	"""
 	for i in range (0,matrice["n"]):
 		yield matrice[(i,m)]
 
 def parcourir_ligne (matrice,n):
+	""" Générateur qui parcour une ligne de la matrice
+		
+		@n : int = la ligne
+		
+		@return : generator
+	"""
 	for j in range (0,matrice["m"]):
 		yield matrice[(n,j)]
 
 def parcourir_lignes (matrice):
+	""" Générateur qui parcour les lignes une à une 
+		et donne des listes (une par ligne)
+		
+		@return : generator
+	"""
 	for i in range (0,matrice["n"]):
 		l = []
 		for j in range (0,matrice["m"]):
@@ -47,6 +74,11 @@ def parcourir_lignes (matrice):
 		yield l
 
 def parcourir_colonnes (matrice):
+	""" Générateur qui parcour les colonnes et donne 
+		des listes
+		
+		@return : generator
+	"""
 	for j in range (0, matrice["m"]):
 		l = []
 		for i in range (0,matrice["n"]):
@@ -54,13 +86,34 @@ def parcourir_colonnes (matrice):
 		yield l
 	
 def set (matrice,i,j,val):
+	"""" Définit la valeur dans une case 
+		
+		@i : int = ligne
+		@j : int = colonne
+		@v : ? = valeur
+		
+		@return : None
+	"""
 	matrice[(i,j)] = val
 
 def get (matrice,i,j):
+	""" Retourne la valeur dans une cellule
+		
+		@i : int = ligne
+		@j : int = colonne
+		
+		@return . ?
+	"""
 	return matrice[(i,j)]
 
 def display (matrice,univers):
-	
+	""" Une fonction qui affiche une matrice 
+		à l'écran ... sans utiliser iconsole ?!
+		
+		@univers : [str ...] = nom des colonnes 
+		
+		@return : None
+	"""
 	print (" |" + "|".join (univers))
 	
 	tailles = [len (k) for k in univers]
