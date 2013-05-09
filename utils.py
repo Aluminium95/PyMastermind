5daf2a05b766b050384f52d63c09b11a7a249cdc
Aluminium95 <lopezaliaume@gmail.com>
2013-05-09 17:38:34 +0200

    Ajout de fonctions de premier ordre utiles

---
+++ utils.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+# Fichier d'utilitaires 
+# qui permet de regrouper 
+# des fonctions généralistes
+# utiles au programme
+
+def bi_map (f,a,b):
+	""" 
+		- f
+		- (a b c d ...)
+		- (e f g h ...)
+		-> (f(a,e) f(b,f) f(c,g) f(d,h) ...)
+	"""
+
+	k = 0
+	S = []
+
+	while k < len (a) and k < len (b):
+		S.append (f (a[k],b[k]))
+		k += 1
+
+	return S
+
+def foldl (f,n,l):
+	""" fonction, neutre, liste
+		
+		[a, b, c, d]
+		f(d,f(c,f(b,f(a,n))))
+	"""
+
+	for i in l:
+		n = f (n,i)
+	
+	return n
+
+
