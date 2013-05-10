917ea660cd8c1a58738e77b5f9b586e2f6596895
Aluminium95 <lopezaliaume@gmail.com>
2013-05-10 16:35:24 +0200

    Ajout d'une fonction de parcours !

--- themes.py
+++ themes.py
@@ -20,6 +20,20 @@ class ThemeCorrompu (Exception):
 
 # FIN EXCEPTIONS
 
+def maximum ():
+	""" Retourne le thème maximal """
+	try:
+		max_theme = persistance.get_propriete ("backgrounds","theme:max")
+	except persistance.CleInvalide:
+		pass
+	except persistance.FichierInvalide:
+		pass
+	else:
+		try:
+			return int (max_theme)
+		except ValueError:
+			raise persistance.ValeurInvalide ("backgrounds","theme:max")
+
 def choix_theme (nbr_theme = 1):
 	""" Sélectionne un thème (modifie le fichier de configuration)
 		
@@ -42,14 +56,31 @@ def choix_theme (nbr_theme = 1):
 	
 	persistance.set_propriete ("backgrounds","theme:courant",str (nbr_theme))
 
+def parcourir_themes_opts (*args):
+	""" Crée la liste des thèmes avec 
+		des options intéressantes 
+	"""
+	for i in range (0,maximum () + 1):
+		l = []
+		for j in args:
+			try:
+				s = "theme:{0}:{1}".format (i,j)
+				l.append (persistance.get_propriete ("backgrounds",s))
+			except:
+				pass # RàF
+		yield l
+		
+def liste_themes_opts (*args):
+	return list (parcourir_themes_opts (*args))
+		
 def liste_themes ():
 	# C'est con ... ça génère ["1", "2", ..., "max"]
-	max_theme = persistance.get_propriete ("backgrounds", "theme:max")
+	max_theme = maximum ()
 	
 	l = []
 
 	i = 1
-	while i <= int (max_theme):
+	while i <= max_theme:
 		l.append (str (i))
 		i += 1
 	return l
