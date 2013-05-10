dd99603a9ded1424a5e47ec54c2c17d6e0114c69
Aluminium95 <lopezaliaume@gmail.com>
2013-05-10 16:26:21 +0200

    Supression des thèmes d'affichage

--- affichage.py
+++ affichage.py
@@ -139,41 +139,6 @@ def loose (code):
 	texte ("La solution était","important")
 
 
-def choix_theme(nbr_theme = 1):
-	""" Sélectionne un thème (modifie le fichier de configuration)
-		
-		@nbr_theme : int = le numéro du nouveau thème sélectionné
-		
-		@return : None
-		
-		@throw :
-			persistance.CleInvalide
-			persistance.FichierInvalide
-			ValueEror # si jamais il y a une valeur erronée dans backgrounds -> theme:max
-	"""
-	if not isinstance (nbr_theme, int):
-		nbr_theme = 1
-	
-	max_theme = persistance.get_propriete ("backgrounds","theme:max")
-	
-	if nbr_theme > int (max_theme):
-		nbr_theme = 1
-	
-	persistance.set_propriete ("backgrounds","theme:courant",str (nbr_theme))
-
-def liste_themes ():
-	# C'est con ... ça génère ["1", "2", ..., "max"]
-	max_theme = persistance.get_propriete ("backgrounds", "theme:max")
-	
-	l = []
-
-	i = 1
-	while i <= int (max_theme):
-		l.append (str (i))
-		i += 1
-	return l
-
-
 def afficher_couleurs(nbr_case,couleurs,answer):
 	""" dessine une ligne du plateau du mastermind avec en argument les donnée envoyé par les autres modules:
     	les couleurs saisies par l'utilisateur et la réponse de l'ordinateur à partir du code secret. Composée de la fonction carré et de la fonction answer
@@ -233,6 +198,7 @@ def generer_score():
 		texte (str (i))
 		yield
 	
+	# TODO: utilisation de scores.py
 	for i in range(0,5):
 		down()
 		score = persistance.get_propriete ("scores", str (i) + ":score")
