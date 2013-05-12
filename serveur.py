29879ec9bc5d82c8c1e4b30f076f2c18c52cd748
Aluminium95 <lopezaliaume@gmail.com>
2013-05-12 11:15:17 +0200

    Amélioration du protocole ... On commence à avoir un truc bien !

--- serveur.py
+++ serveur.py
@@ -7,15 +7,13 @@
 # distant !
 #
 # Protocole :
-#   Entrées :
+#   Entrées (Client envoie) :
 # 	  (t)(.*) : chatter (envoyer du texte)
-#     (>)(.*) : envoie une réponse au seveur
-# 	  (<)(.*) : pose une question au serveur
-#     (h)     : demande l'aide
-#   Sorties :
-#	  (>)(.*) : afficher du texte sur la console du client
-#     (t)(.*) : envoyer un message (chat) au client
-#	  (<)(.*) : pose une question (attends une réponse)
+# 	  (<)(.*) : commande (demande un truc au serveur)
+#   Sorties (Client reçoit) :
+#	  (t)(.*) : chatter (le serveur envoie du texte)
+#	  (>)[(.+)](*.) : le serveur répond (\1 : état, \2, réponse)
+#
 
 HOST = 'localhost' # fonctionne ?
 PORT = 3500 
@@ -23,49 +21,73 @@ PORT = 3500
 import socket, sys, threading
 
 class ClientProtocol:
+	""" Protocole basique de comunication avec 
+		le client, met en place toutes les fonctions
+		bas niveau pour communiquer, permettant une 
+		abstraction des choses comme l'encodage, 
+		la gestion des autres clients etc ...
+	"""
 	messages = [] # Liste des messages
 	users = {} # Users ...
-	def __init__ (self,identifiant):
+	def __init__ (self,identifiant,connexion):
 		
 		self.identifiant = identifiant
+		self.connexion = connexion
+		
+		ClientProtocol.users[identifiant] = connexion
 
-	def parse (self,msg):
-		pass
-	
+	def receive (self):
+		return self.connexion.recv (1024).decode ("utf-8")
+
 	def broadcast (self,msg):
 		for i,j in ClientProtocol.users.items ():
 			if i != self.identifiant:
-				j.send (msg.encode ("utf-8"))
+				self.send (j,msg)
 
-	def send (self,t,msg):
-		pass
+	def send (self,conn,msg):
+		if msg[-1] != "\n":
+			msg += "\n" # On termine par un \n
+		conn.send (msg.encode ("utf-8"))
+
+	def reply (self,msg):
+
+		self.send (self.connexion, msg)
 
 clients = {} # Tableau des clients
 
 # Thread qui gère un client
 class ThreadClient (threading.Thread,ClientProtocol):
+	""" Thread qui gère la connexion avec un client,
+		c'est tout ce qui fait le lien entre 
+		le client et le serveur
+	"""
 	def __init__ (self, conn):
 		threading.Thread.__init__ (self)
 
-		ClientProtocol.__init__ (self, self.getName ())
-
-		self.connection = conn # Enregistre la connection client
-		
-		ClientProtocol.users[self.getName ()] = conn
+		ClientProtocol.__init__ (self, self.getName (), conn)
 
 	def run (self):
 		while True:
-			m = self.connection.recv (1024)
-			m = m.decode ("utf-8")
-			# self.connection.send ("Message reçu !".encode ("utf-8"))
+			m = self.receive () # Attends une reponse !
 			
-			self.broadcast (m)
-			
-			self.connection.send ("Message envoyé !\n".encode ("utf-8"))
+			context,message = m[0:3],m[3:]
 
+			if context == "(<)": # Le client fait une requête
+				self.send ("(>)[None]Votre réponse")
+			elif context == "(t)": # Le client veut chatter
+				self.broadcast ("(t)" + message) # Fait circuler le message (m)
+			else:
+				print ("Mauvaise gestion des paquets ...")
 		self.connection.close ()
 
 class ThreadServeur (threading.Thread):
+	""" Thread du serveur .... C'est tout ce qui 
+		est au niveau de la gestion de 
+		la réception des clients et du démarrage 
+		de nouveaux threadsClients
+		
+		Il possède donc la Socket
+	"""
 	def __init__ (self):
 		threading.Thread.__init__ (self)
 		
